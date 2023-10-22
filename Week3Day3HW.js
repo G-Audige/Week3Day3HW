ccbfcc202d0d7cfbd942e0c95eed30085622c659
G-Audige <gloriaaudige@gmail.com>
2023-10-21 21:12:29 -0400

    Completed required tasks

--- Week3Day3HW.js
+++ Week3Day3HW.js
@@ -1,21 +1,20 @@
 // Static Methods and Properties
-class Governor {
-    constructor () {    
-    this.state = 'Alaska';
-    this.party = 'Republican';
-    this.popularity = 89;
+class Governor {   
+    static state = 'Alaska';
+    static party ='Republican';
+    static popularity = 82
+    static greeting(state){
+        console.log("Hello " + state + "!")
     }
-    greeting(){
-        console.log("Hello " + this.state + "!")
+    static campaigning(pop,state) {
+        console.log(pop + "% of " + state + "'s citizen's approve of our leadership.")
     }
 }
-
-const executive = new Governor
-console.log(executive.state)
-console.log(executive.party)
-console.log(executive.popularity)
-executive.greeting()
-
+console.log(Governor.state)
+console.log(Governor.party)
+console.log(Governor.popularity)
+Governor.greeting(Governor.state)
+Governor.campaigning(Governor.popularity,Governor.state)
 
 // Inheritance
 class Person {
@@ -38,17 +37,32 @@ class Person {
 }
 
 class PostalWorker extends Person {
-
+    working() {
+        console.log("*Sorting packages")
+    }
 }
-const pw1 = new PostalWorker("Blonde", "Blue", "Richie", "November 4th", 32)
+const pw1 = new PostalWorker("Blue", "Blonde", "Richie", "November 4th", 32)
 const pw2 = new PostalWorker("Brown", "Black", "John", "March 17th", 44)
 console.log(pw1)
 pw1.hungry()
+pw1.cold()
 console.log(pw2)
+pw2.working()
 pw2.tired()
 
 class Chef extends Person {
-
+    working() {
+        console.log("*Cooking")
+    }
+    hot() {
+        console.log("It's too hot.")
+    }
 }
-const c1 = new Chef()
-const c2 = new Chef()
+const c1 = new Chef("Black","Brown","Jeffrey","August 21st",40)
+const c2 = new Chef("Blonde", "Brown","Barbara","September 13th", 28)
+console.log(c1)
+c1.hot()
+c1.tired()
+console.log(c2)
+c2.working()
+c2.hungry()
